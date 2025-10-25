af432f1067e1ded2d1b26d3b4a7cf902c3ef0e57
J. Howell <jhowell74@gmail.com>
2025-10-25 09:07:42 -0700

    Updated Environment data

--- js/data_environment.js
+++ js/data_environment.js
@@ -1,58 +1,62 @@
 data_environment_obscurance = [
     {
-        title: "Lightly obscured",
-        icon: "bleeding-eye",
+        title: "Lightly Obscured",
+        icon: "fog",
         subtitle: "Disadvantage on Perception",
         description: "Dim light, patchy fog, moderate foliage",
-        reference: "PHB, pg. 183.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/playing-the-game#ObscuredAreas'>Playing the Game: Obscured Areas, PHB 19</a>; "
+                   + "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#LightlyObscured'>Lightly Obscured, PHB 370</a>",
         bullets: [
-            "Creatures have <b>disadvantage on Wisdom (Perception)</b> checks that rely on sight."
+            "Creatures have Disadvantage on Wisdom (Perception) checks that rely on sight."
         ]
     },
     {
-        title: "Heavily obscured",
-        icon: "lightning-tear",
+        title: "Heavily Obscured",
+        icon: "hidden",
         subtitle: "Effectively blind",
         description: "Darkness, opaque fog, dense foliage",
-        reference: "PHB, pg. 183.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/playing-the-game#ObscuredAreas'>Playing the Game: Obscured Areas, PHB 19</a>; "
+                   + "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#LightlyObscured'>Heavily Obscured, PHB 368</a>",
         bullets: [
-            "A creature in a heavily obscured area effectively suffers from the <b>blinded condition</b>."
+            "A creature in a heavily obscured area effectively suffers from the Blinded condition when trying to see something there."
         ]
     }
 ]
 
 data_environment_light = [
     {
-        title: "Bright light",
-        icon: "star-pupil",
+        title: "Bright Light",
+        icon: "sunbeams",
         subtitle: "Normal vision",
-        description: "Bright light lets most creatures see normally",
-        reference: "PHB, pg. 183.",
+        description: "Bright Light lets most creatures see normally",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/playing-the-game#Light'>Playing the Game: Light, PHB 19</a>; "
+                   + "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#BrightLight'>Bright Light, PHB 362</a>",
         bullets: [
             "Gloomy days still provide bright light, as do torches, lanterns, fires, and other sources of illumination within a specific radius."
         ]
     },
     {
-        title: "Dim light",
-        icon: "semi-closed-eye",
+        title: "Dim Light",
+        icon: "wolf-howl",
         subtitle: "Lightly obscured",
         description: "Dim light, also called shadows",
-        reference: "PHB, pg. 183.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/playing-the-game#Light'>Playing the Game: Light, PHB 19</a>; "
+                   + "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#DimLight'>Dim Light, PHB 366</a>",
         bullets: [
-            "Creates a <b>lightly obscured</b> area.",
-            "An area of dim light is usually a boundary between a source of bright light, such as a torch, and surrounding darkness.",
-            "The soft light of twilight and dawn also counts as dim light. A particularly brilliant full moon might bathe the land in dim light."
+            "Creates a Lightly Obscured area.",
+            "An area of Dim Light is usually a boundary between Bright Light and surrounding Darkness.",
+            "The soft light of twilight and dawn also counts as Dim Light. A full moon might bathe the land in dim light."
         ]
     },
     {
         title: "Darkness",
         icon: "worried-eyes",
         subtitle: "Heavily obscured",
-        description: "Darkness creates a heavily obscured area",
-        reference: "PHB, pg. 183.",
+        description: "Darkness creates a Heavily Obscured area",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/playing-the-game#Light'>Playing the Game: Light, PHB 19</a>; "
+                   + "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Darkness'>Darkness, PHB 365</a>",
         bullets: [
-            "Creates a <b>heavily obscured</b> area.",
-            "Characters face darkness outdoors at night (even most moonlit nights), within the confines of an unlit dungeon or a subterranean vault, or in an area of magical darkness."
+            "Characters face Darkness outdoors at night (even most moonlit nights), within the confines of an unlit dungeon, or in an area of magical darkness."
         ]
     }
 ]
@@ -60,35 +64,50 @@ data_environment_light = [
 data_environment_vision = [
     {
         title: "Blindsight",
-        icon: "one-eyed",
+        icon: "blindfold",
         subtitle: "Perceive without sight",
-        description: "Perceive your surroundings without relying on sight, within a certain radius",
-        reference: "PHB, pg. 183.",
+        description: "Perceive your surroundings without relying on physical sight, within a specified range",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Blindsight'>Blindsight, PHB 361</a>",
         bullets: [
-            "Creatures without eyes, such as oozes, and creatures with echolocation or heightened senses, such as bats and true dragons, have this sense."
+            "You can see anything that isn't behind Total Cover even if you have the Blinded condtion or are in Darkness.",
+            "You can see something that has the Invisible condition."
         ]
     },
     {
         title: "Darkvision",
-        icon: "semi-closed-eye",
-        subtitle: "Limited vision in darkness",
-        description: "A creature with Darkvision can see better in the dark or low light conditions, within a certain radius",
-        reference: "PHB, pgs. 183-184.",
+        icon: "night-vision",
+        subtitle: "Limited vision in Darkness",
+        description: "A creature with Darkvision can see better in the dark or low light conditions, within a specified range",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Darkness'>Darkvision, PHB 365</a>",
         bullets: [
-            "Within a specified range, a creature with darkvision can <b>see in darkness as if the darkness were dim light</b>, so areas of darkness are only lightly obscured as far as that creature is concerned.",
-            "However, the creature can’t discern color in darkness, only shades of gray.",
-            "Many creatures in the worlds of D&D, especially those that dwell underground, have darkvision."
+            "You can see in Dim Light as if it were Bright Light and in Darkness as if it were Dim Light.",
+            "You disern colors in Darkness only as shades of gray."
+        ]
+    },
+    {
+        title: "Tremorsense",
+        icon: "awareness",
+        subtitle: "See in Darkness",
+        description: "A creature with Tremorsense can locate creatures and moving objects within a specific range",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Tremorsense'>Tremorsense, PHB 377</a>",
+        bullets: [
+            "You must must be in contact with the same surface, or the same liquid, as anything you are detecting.",
+            "You can't detect anything in the air",
+            "Tremorsense doesn't count as a form of sight."
         ]
     },
     {
         title: "Truesight",
-        icon: "eye-shield",
-        subtitle: "See in darkness",
-        description: "A creature with truesight can see everything in its true form, independent of the environment",
-        reference: "PHB, pg. 184.",
+        icon: "third-eye",
+        subtitle: "See in Darkness",
+        description: "The vision of a creature with Truesight is enhanced within a specified range",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Truesight'>Truesight, PHB 377</a>",
         bullets: [
-            "A creature with truesight can, out to a specific range, see in normal and magical darkness, see invisible creatures and objects, automatically detect visual illusions and succeed on saving throws against them, and perceives the original form of a shapechanger or a creature that is transformed by magic.",
-            "Furthermore, the creature can see into the Ethereal Plane."
+            "You can see in normal and magical Darkness.",
+            "You see creatures and objects that have the Invisible condition.",
+            "Visual illusions appear transparent to you. You automatically succeed on saving throws against them.",
+            "You discern the true form of any creature or object you see that has been transformed by magic",
+            "You see into the Ethereal Plane."
         ]
     }
 ]
@@ -96,36 +115,36 @@ data_environment_vision = [
 data_environment_cover = [
     {
         title: "Half cover",
-        icon: "broken-shield",
+        icon: "brick-pile",
         subtitle: "Low wall, furniture, creatures",
-        description: "A target has half cover if an obstacle blocks at least half of its body",
-        reference: "PHB, pg. 196.",
+        description: "A target has Half Cover if an obstacle blocks at least half of its body",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Cover'>Cover, PHB 364</a>",
         bullets: [
             "The obstacle might be a low wall, a large piece of furniture, a narrow tree trunk, or a creature, whether that creature is an enemy or a friend.",
-            "A target with half cover has a <b>+2 bonus to AC and Dexterity saving throws</b>.",
-            " If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+            "A target with Half Cover has a +2 bonus to AC and Dexterity saving throws.",
+            "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
         ]
     },
     {
         title: "Three-quarters cover",
-        icon: "cracked-shield",
+        icon: "broken-wall",
         subtitle: "Portcullis, arrow slit",
-        description: "A target has three-quarters cover if about three-quarters of it is covered by an obstacle",
-        reference: "PHB, pg. 196.",
+        description: "A target has Three-quarters cover if about Three-Quarters of it is covered by an obstacle",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Cover'>Cover, PHB 364</a>",
         bullets: [
             "The obstacle might be a portcullis, an arrow slit, or a thick tree trunk.",
-            "A target with three-quarters cover has a <b>+5 bonus to AC and Dexterity saving throws</b>.",
-            " If a target is behind multiple sources of cover, only the most protective degree of cover applies"
+            "A target with Three-Quarters cover has a +5 bonus to AC and Dexterity saving throws.",
+            "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
         ]
     },
     {
         title: "Full cover",
-        icon: "shield",
+        icon: "brick-wall",
         subtitle: "Completely concealed",
         description: "A target has total cover if it is completely concealed by an obstacle",
-        reference: "PHB, pg. 196.",
+        reference: "<a href='https://www.dndbeyond.com/sources/dnd/br-2024/rules-glossary#Cover'>Cover, PHB 364</a>",
         bullets: [
-            "A target with total cover <b>can’t be targeted directly</b> by an attack or a spell, although some spells can reach such a target by including it in an area of effect.",
+            "A target with total cover can’t be targeted directly by an attack or a spell, although some spells can reach such a target by including it in an area of effect.",
             "If a target is behind multiple sources of cover, only the most protective degree of cover applies"
         ]
     }
